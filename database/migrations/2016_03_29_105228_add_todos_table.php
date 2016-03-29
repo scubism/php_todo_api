9aa794d529b6e802d453f9937473ff95b1a445d5
DucHa <ducha.ptit.ltt@gmail.com>
2016-03-29 20:26:25 +0900

    Add migration files

---
+++ database/migrations/2016_03_29_105228_add_todos_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddTodosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+	if (Schema::hasTable('todos')) {
+            Schema:create('todos', function (Blueprint $table) {
+                $table->increments('id');
+                $table->string('title',255);
+                $table->dateTime('duedate');
+                $table->string('color', 255);
+                $table->integer('group_id');
+            });
+        }
+	Schema::table('todos', function($table) {
+            $table->dateTime('duedate')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+	Schema::dropIfExists('todos');
+    }
+}
