48dd71b9f561250f2de892116ab77bb6c0e4a5a1
HuyNQ <huy@huynq.net>
2016-03-31 23:08:58 +0900

    Upgrade php from 5.6 to 7

--- Dockerfile
+++ Dockerfile
@@ -1,24 +1,18 @@
-FROM scubism/php56-apache:0.1
+FROM php:7-fpm
 
-RUN rm -rf /var/www/html
+RUN apt-get update \
+    && apt-get install nano bash -y \
+    && rm -rf /var/lib/apt/lists/*
 
-ADD php_todo_api.conf /var/www/php_todo_api.conf
-
-RUN cat /var/www/php_todo_api.conf >> /etc/httpd/conf/httpd.conf
-
-RUN mkdir /var/www/php_todo_api
-
-WORKDIR /var/www/php_todo_api
-
-COPY . .
+COPY . /var/www/html
 
 RUN php -r "readfile('https://getcomposer.org/installer');" > composer-setup.php && \
-php -r "if (hash('SHA384', file_get_contents('composer-setup.php')) === '41e71d86b40f28e771d4bb662b997f79625196afcca95a5abf44391188c695c6c1456e16154c75a211d238cc3bc5cb47') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"  && \
-php composer-setup.php --install-dir=/usr/bin --filename=composer && \
-php -r "unlink('composer-setup.php');"
+    php -r "if (hash('SHA384', file_get_contents('composer-setup.php')) === '41e71d86b40f28e771d4bb662b997f79625196afcca95a5abf44391188c695c6c1456e16154c75a211d238cc3bc5cb47') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"  && \
+    php composer-setup.php --install-dir=/usr/bin --filename=composer && \
+    php -r "unlink('composer-setup.php');"
 
 COPY docker-entrypoint.sh /
 
-EXPOSE 80 443
+EXPOSE 9000
 
 ENTRYPOINT ["/docker-entrypoint.sh"]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,25 +1,16 @@
-#!/bin/env bash
+#!/bin/sh
 set -e
 
-APP_ENV=${APP_ENV:-'local'}
+ENV=${APP_ENV:-'local'}
 RANDOM_KEY=`< /dev/urandom tr -dc A-Za-z0-9 | head -c${1:-32};echo;`
 
-if [ $APP_ENV == 'local' ]; then
+if [ $ENV = 'local' ]; then
   APP_DEBUG=true
-  HTTPD_STATE="BACKGROUD"
-  if getent passwd devuser > /dev/null 2>&1; then
-    echo 'User devuser is existed'
-  else
-    adduser --system --uid=$(stat -c %u .) devuser
-    sed -i 's/User apache/User devuser/g' /etc/httpd/conf/httpd.conf
-    sed -i 's/Group apache/Group devuser/g' /etc/httpd/conf/httpd.conf
-  fi
-  chown root:devuser -R . && chmod 775 -R .
 else
   APP_DEBUG=false
-  HTTPD_STATE="FOREGROUND"
-  chown root:apache -R . && chmod 775 -R .
 fi
+
+chmod 775 -R /var/www/html
 
 if [ ! -f ".env" ]; then
   sed -e "s/DB_HOST=localhost/DB_HOST=${DB_HOST}/g" \
@@ -27,18 +18,11 @@ if [ ! -f ".env" ]; then
     -e "s/DB_DATABASE=homestead/DB_DATABASE=${DB_NAME}/g" \
     -e "s/DB_USERNAME=homestead/DB_USERNAME=${DB_USER}/g" \
     -e "s/DB_PASSWORD=secret/DB_PASSWORD=${DB_PASSWORD}/g" \
-    -e "s/APP_ENV=local/APP_ENV=${APP_ENV}/g" \
+    -e "s/APP_ENV=local/APP_ENV=${ENV}/g" \
     -e "s/APP_DEBUG=true/APP_DEBUG=${APP_DEBUG}/g" \
     -e "s/APP_KEY=SomeRandomKey!!!/APP_KEY=${RANDOM_KEY}/g" \
     < .env.example \
     > .env
 fi
 
-if [ "$1" = '' ]; then
-  rm -rf /run/httpd/* /tmp/httpd*
-  set -- /usr/sbin/apachectl -D $HTTPD_STATE
-fi
-
-echo "$@"
-exec "$@"
-
+exec "php-fpm"
