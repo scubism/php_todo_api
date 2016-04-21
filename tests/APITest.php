188ef39dafa4be2c6ce6a036ca664c5aaaf0227b
DucHa <ducha.ptit.ltt@gmail.com>
2016-04-21 09:51:45 +0700

    Fix unit test

--- tests/APITest.php
+++ tests/APITest.php
@@ -6,17 +6,19 @@ class APITest extends TestCase {
 	public function testAllTodos() {
 		$this->json('GET', '/v1/todos')
 			->seeJson([
-				'id' => '1'
+				'id' => 2
 			]);
 	}
 
 	public function testViewTodo() {
-		$response = $this->call('GET', '/v1/todos/0');
-		$this->assertFalse($response);
-
-		$this->json('GET', '/v1/todos/1')
+		$this->json('GET', '/v1/todos/0')
 			->seeJson([
-				'id' => '1'
+				'error' => 'Not found.'
+			]);
+
+		$this->json('GET', '/v1/todos/2')
+			->seeJson([
+				'id' => 2
 			]);
 	}
 
@@ -27,9 +29,8 @@ class APITest extends TestCase {
 			'todo_groups_id' => '1'])
 			->seeJson([
 				'title' => 'Test Todo 1',
-				'duedate' => '2016-04-20 10:42', 
 				'color' => 'black', 
-				'todo_groups_id' => '1'
+				'todo_groups_id' => 1
 			]);
 
 		$response = $this->call('POST', '/v1/todos', ['title' => 'Test Todo 1', 
@@ -37,7 +38,6 @@ class APITest extends TestCase {
 			'color' => 'black', 
 			'todo_groups_id' => '1']);
 		$this->assertEquals(200, $response->status());
-		$this->assertEquals('application/json', $response->header('Content-Type'));
 	}
 
 	public function testUpdateTodo() {
@@ -47,43 +47,52 @@ class APITest extends TestCase {
 			'color' => 'black', 
 			'todo_groups_id' => '1'
 		]);
-		$this->assertEquals(422, $response->status());
-
-		$this->json('PUT', '/v1/todos/1', ['title' => 'Test Todo 1 Updated', 
+		$this->assertEquals(404, $response->status());
+		
+		$this->json('PUT', '/v1/todos/0', ['title' => 'Test Todo 1 Updated', 
 			'duedate' => '2016-04-20 10:42', 
 			'color' => 'black', 
 			'todo_groups_id' => '1'])
 			->seeJson([
+				'error' => 'Not found.'
+			]);
+
+		$this->json('PUT', '/v1/todos/2', ['title' => 'Test Todo 1 Updated', 
+			'duedate' => '2016-04-20 10:42', 
+			'color' => 'white', 
+			'todo_groups_id' => '1'])
+			->seeJson([
 				'title' => 'Test Todo 1 Updated',
-				'duedate' => '2016-04-20 10:42', 
-				'color' => 'black', 
-				'todo_groups_id' => '1'
+				'color' => 'white', 
+				'todo_groups_id' => 1
 			]);
 	}
 
 	public function testDeleteTodo() {
-		$response = $this->call('PUT', '/v1/todos/0');
-		$this->assertEquals(422, $response->status());
+		$response = $this->call('DELETE', '/v1/todos/0');
+		$this->assertEquals(404, $response->status());
 
-		$this->json('PUT', '/v1/todos/1')
+		$this->json('DELETE', '/v1/todos/0')
 			->seeJson([
-				'title' => 'Test Todo 1 Updated',
-				'duedate' => '2016-04-20 10:42', 
+				'error' => 'Not found.'
+			]);
+		$this->json('DELETE', '/v1/todos/12')
+			->seeJson([
+				'title' => 'Test Todo 1',
 				'color' => 'black', 
-				'todo_groups_id' => '1'
+				'todo_groups_id' => 1
 			]);
 	}
 
 	public function testMoveTodo() {
-		$response = $this->call('PUT', '/v1/todos/0/move', ['prior_sibling_id' => 2]);
-		$this->assertEquals(422, $response->status());
-
-		$this->json('PUT', '/v1/todos/1/move', ['prior_sibling_id' => 2])
+		$this->json('POST', '/v1/todos/0/move', ['prior_sibling_id' => 2])
 			->seeJson([
-				'title' => 'Test Todo 1 Updated',
-				'duedate' => '2016-04-20 10:42', 
-				'color' => 'black', 
-				'todo_groups_id' => '1'
+				'error' => 'Not found.'
+			]);
+
+		$this->json('POST', '/v1/todos/3/move', ['prior_sibling_id' => 2])
+			->seeJson([
+				'id' => 3
 			]);
 	}
 }
